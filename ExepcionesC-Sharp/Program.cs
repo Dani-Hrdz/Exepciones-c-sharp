ee721c91dc2c986b239bbaea8608f5f69cd8867b
Dani-Hrdz <105619891+Dani-Hrdz@users.noreply.github.com>
2022-05-19 11:40:37 -0700

    modificacion1
    programa a entregar

---
+++ ExepcionesC-Sharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExepcionesC_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("ingresa un numero: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("ingresa otro numero: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+                Console.ReadLine();
+            }
+            catch(DivideByZeroException f)
+            {
+                Console.WriteLine(f.Message);
+            }
+            catch(FormatException a)
+            {
+                Console.WriteLine(a.Message);
+            }
+            finally { }
+        }
+    }
+}
